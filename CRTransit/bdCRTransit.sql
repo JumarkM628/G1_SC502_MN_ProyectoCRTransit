6bbfcca26e4a6a6e8f965eba0519a882e804ec49
JumarkM628 <jmiranda59803@ufide.ac.cr>
2025-12-09 17:04:27 -0600

    Se actualizo la base de datos a utilizar

--- CRTransit/bdCRTransit.sql
+++ CRTransit/bdCRTransit.sql
@@ -8,3 +8,35 @@ CREATE TABLE alertas (
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 SELECT * FROM alertas;
+
+CREATE TABLE usuario(
+	idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(25) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ruta (
+	idRuta INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(25) NOT NULL,
+    origen VARCHAR(25) NOT NULL,
+    destino VARCHAR(25) NOT NULL,
+    duracion int NOT NULL);
+
+CREATE  TABLE historial(
+	idViaje INT AUTO_INCREMENT PRIMARY KEY,
+    idUsuario int,
+    idRuta int,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    duracion int,
+    FOREIGN KEY (idUsuario) REFERENCES usuario (idUsuario),
+    FOREIGN KEY (idRuta) REFERENCES ruta (idRuta));
+    
+CREATE TABLE autobuses(
+	idBus INT AUTO_INCREMENT PRIMARY KEY,
+    idRuta int,
+    placa VARCHAR(25) NOT NULL,
+    FOREIGN KEY (idRuta) REFERENCES ruta (idRuta)
+    );
+
